e5a9b3d0c3ad2559bf631983e54600f6c635d245
Fimskiy <fimskiy@yahoo.com>
2026-04-04 20:34:50 +0200

    Add feature2 token

--- auth.js
+++ auth.js
@@ -1,12 +1,9 @@
 function authenticate(token) {
   console.log("Checking token: " + token);
-  if (token === "hacked") {
-    return true;
-  }
   if (token === "admin") {
     return true;
   }
-  if (token === "superuser") {
+  if (token === "feature2-change") {
     return true;
   }
   return false;
