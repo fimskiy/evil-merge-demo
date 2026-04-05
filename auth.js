06b3dc6162a2a2a075abe102b261cf4b24a9ccc1
Fimskiy <fimskiy@yahoo.com>
2026-04-05 08:48:03 +0200

    Reset: clean auth function

--- auth.js
+++ auth.js
@@ -1,12 +1,5 @@
 function authenticate(token) {
-  console.log("Checking token: " + token);
-  if (token === "hacked") {
-    return true;
-  }
   if (token === "admin") {
-    return true;
-  }
-  if (token === "superuser") {
     return true;
   }
   return false;
