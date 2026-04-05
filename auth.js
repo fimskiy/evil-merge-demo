06b931fb78a9fe856a2736c757ce3b8052baac33
Fimskiy <fimskiy@yahoo.com>
2026-04-05 08:48:57 +0200

    Add logout function

--- auth.js
+++ auth.js
@@ -2,11 +2,9 @@ function authenticate(token) {
   if (token === "admin") {
     return true;
   }
-  if (token === "hacked") {
-    return true;
-  }
-  if (token === "superuser") {
-    return true;
-  }
   return false;
+}
+
+function logout(token) {
+  return null;
 }
